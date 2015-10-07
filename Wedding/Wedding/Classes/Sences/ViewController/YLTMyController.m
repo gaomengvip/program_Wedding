0916f4cee2d536aebc76ee8c40d9dcb28937f73c
lanou3g <lanou3g@lanou3gdeMac-mini-3.local>
2015-10-07 21:15:12 +0800

    no message

---
+++ Wedding/Wedding/Classes/Sences/ViewController/YLTMyController.m
@@ -0,0 +1,104 @@
+//
+//  YLTMyController.m
+//  Wedding
+//
+//  Created by lanou3g on 15/10/7.
+//  Copyright (c) 2015年 蓝鸥科技. All rights reserved.
+//
+
+#import "YLTMyController.h"
+#import "YLTMyCell.h"
+
+@interface YLTMyController ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+
+@end
+
+@implementation YLTMyController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc]init];
+    flowLayout.itemSize = CGSizeMake(self.view.frame.size.width, 30);
+    flowLayout.minimumLineSpacing = 0;
+    flowLayout.minimumInteritemSpacing = 0;
+    flowLayout.sectionInset = UIEdgeInsetsMake(20, 20, 20, 20);
+    flowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    flowLayout.headerReferenceSize = CGSizeMake(self.view.frame.size.width,150);
+
+
+    UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 700) collectionViewLayout:flowLayout];
+    collectionView.delegate = self;
+    collectionView.dataSource = self;
+    collectionView.backgroundColor = [UIColor whiteColor];
+    
+    //注册cell
+    [collectionView registerNib:[UINib nibWithNibName:@"YLTMyCell" bundle:nil] forCellWithReuseIdentifier:@"cell"];
+    
+    //注册头
+    [collectionView registerNib:[UINib nibWithNibName:@"YLTMyHeaderView" bundle:nil] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"myheader"];
+    [collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"header"];
+    
+    
+    //注册区尾
+    [collectionView registerClass:[UICollectionElementKindSectionFooter class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footer"];
+    
+    
+    [self.view addSubview:collectionView];
+
+}
+
+
+#pragma mark ----collection的代理方法-----
+
+//设置分区的个数
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 5;
+}
+
+
+
+//设置条目的个数
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    if (section == 0) {
+        return 2;
+    }else if (section == 1){
+        return 4;
+    }else if (section == 2){
+        return 1;
+    }else{
+        return 2;
+    }
+
+}
+
+//设置cell
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    YLTMyCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    if (indexPath.section == 0) {
+        if (indexPath.row == 0) {
+          
+        }
+    }
+    return cell;
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
