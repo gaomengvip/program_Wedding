3ef288107ff03a05f17f59b3b12b2c5425aad636
lanou3g <lanou3g@lanou3gdeMac-mini-8.local>
2015-10-08 09:50:16 +0800

    [fix]东

---
+++ Wedding/Wedding/Classes/Sences/ViewController/LYCHeader4BrideSayFirstView.h
@@ -0,0 +1,13 @@
+//
+//  LYCHeader4BrideSayFirstView.h
+//  Wedding
+//
+//  Created by lanou3g on 15/10/7.
+//  Copyright © 2015年 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYCHeader4BrideSayFirstView : UICollectionReusableView
+
+@end
