e5070136b5f3a52f305cecc78c40e3b72ef4cd7b
lanou3g <lanou3g@lanou3gdeMac-mini-91.local>
2015-10-08 10:54:47 +0800

    [update]lianxi

--- Wedding/Wedding/Classes/AppDelegate/AppDelegate.m
+++ Wedding/Wedding/Classes/AppDelegate/AppDelegate.m
@@ -19,7 +19,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
     self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[HomePageController new]];
+    
+    
+    
+    
+    
+    
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];         
     return YES;
